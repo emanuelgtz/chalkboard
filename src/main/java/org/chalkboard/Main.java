7947c15b0d5ca26e77adcaf7a9987e713c9af3e8
emanuel gutierrez <manudatadev@icloud.com>
2024-08-05 00:08:48 -0600

    creating security config

--- src/main/java/org/chalkboard/Main.java
+++ src/main/java/org/chalkboard/Main.java
@@ -1,15 +1,20 @@
 package org.chalkboard;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@SpringBootConfiguration
 public class Main {
   public static void main(String[] args) {
 
     System.out.println("Application running");
-
+    SpringApplication.run(Main.class,args);
   }
-
 
 
 
